8d8d8a37b3d70146311a778978376cf0e3e72184
Maruthi Prasad <pmaruthiprasad16@gmail.com>
2024-04-06 11:12:57 +0530

    'requirements_added'

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # End-to-End-NLP-Project
+
+## How to run?
+
+```bash
+conda create -n hate python=3.8 -y 
+```
+
+```bash
+conda activate hate 
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Maruthi Prasad",
+    author_email="pmaruthiprasad22@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
